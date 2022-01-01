939f892d1179ebd3b32555f1efb726ff8dcc0996
AloofSkip <nic.veloce@gmail.com>
2021-12-31 20:32:01 -0800

    Add basic portfolio pages

---
+++ pages/portfolio/boeing-speckle.tsx
@@ -0,0 +1,152 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import HeroBlockMini from "../../components/HeroBlockMini";
+import PageWrapper from "../../components/PageWrapper";
+import { Alert, AlertIcon, Box, Flex, Tag, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import homeImage from "../../public/images/portfolio/Boeing-Speckle.jpg";
+import BodyLink from "../../components/BodyLink";
+import BodyHeading from "../../components/BodyHeading";
+import BodyButton from "../../components/BodyButton";
+
+const BoeingSpeckle: NextPage = () => {
+    const tags = ["JavaScript", "React", "MongoDB", "Express", "Node.js"];
+
+    return (
+        <>
+            <Head>
+                <title>Nic Nolan | Boeing Speckle</title>
+                <meta charSet="UTF-8" />
+                <meta
+                    name="keywords"
+                    content="Nic Nolan, Portfolio, Computer Science, Software Developer, Software Engineer"
+                />
+                <meta
+                    name="description"
+                    content="Portfolio website for Nic Nolan, a Software Developer from the United States"
+                />
+                <meta name="author" content="Nic Nolan" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
+            <PageWrapper>
+                <HeroBlockMini />
+                <Flex
+                    direction={"column"}
+                    gap={8}
+                    p={8}
+                    textAlign={"justify"}
+                    color={"#222"}
+                    w={"100%"}
+                >
+                    <Flex direction={"column"} gap={4}>
+                        <BodyHeading>Boeing Speckle</BodyHeading>
+                        <Flex wrap={"wrap"} gap={2}>
+                            {tags.map((tag) => (
+                                <Tag key={tag} size={"md"}>
+                                    {tag}
+                                </Tag>
+                            ))}
+                        </Flex>
+                        <Text
+                            fontWeight={"300"}
+                            fontSize={"xl"}
+                            color={"#999"}
+                            textAlign={"justify"}
+                        >
+                            Boeing Speckle is an API that scrapes data from BAC
+                            specifications publicly available on the Boeing
+                            D1-4426 website for companies in the Boeing supply
+                            chain. Boeing Speckle allows users to query Boeing
+                            specifications by their document number and return
+                            related information including the document title,
+                            revision level, and revision date. The API is
+                            available as a GUI or by making API calls to return
+                            a JSON response.
+                        </Text>
+                    </Flex>
+                    <Flex direction={"column"} gap={4}>
+                        <Flex gap={4}>
+                            <BodyButton>View Demo</BodyButton>
+                            <BodyButton>View Source</BodyButton>
+                        </Flex>
+                        <Alert status="info" fontSize={"md"}>
+                            <AlertIcon />
+                            This demo uses a Heroku &quot;Hobby&quot;
+                            development server that sleeps after 30 minutes of
+                            inactivity. You may have to wait 10 to 15 seconds
+                            before the demo initially loads.
+                        </Alert>
+                    </Flex>
+
+                    <Flex
+                        position={"relative"}
+                        w={"100%"}
+                        height={"100%"}
+                        direction={"column"}
+                        gap={8}
+                    >
+                        <BodyHeading size={"lg"}>Preview</BodyHeading>
+                        <Flex direction={"column"} position={"relative"}>
+                            {console.log(homeImage)}
+                            <Box
+                                border={"solid 1px #eee"}
+                                borderRadius={8}
+                                overflow={"hidden"}
+                                boxShadow={"lg"}
+                            >
+                                <Image
+                                    src={homeImage}
+                                    alt={
+                                        "Preview of the Boeing Speckle Home Page"
+                                    }
+                                    layout={"responsive"}
+                                    quality={100}
+                                />
+                            </Box>
+                            <Text
+                                fontWeight={"300"}
+                                fontSize={"md"}
+                                color={"#999"}
+                                textAlign={"justify"}
+                                p={[2, 4, 8, 16]}
+                            >
+                                Above is the Boeing Speckle home page. It shows
+                                an example of a specification search using the
+                                graphical user interface (GUI). The results
+                                include the full specification number, revision
+                                letter, title, and date of last revision.
+                            </Text>
+                        </Flex>
+                        <Flex direction={"column"} gap={8}>
+                            <Flex direction={"column"}>
+                                <BodyHeading>Technical Details</BodyHeading>
+                                <Text
+                                    fontWeight={"300"}
+                                    fontSize={"xl"}
+                                    color={"#999"}
+                                    textAlign={"justify"}
+                                >
+                                    The following sections detail the problem
+                                    that this project sought to solve, the
+                                    associated challenges, the ultimate
+                                    solution, and my final thoughts.
+                                </Text>
+                            </Flex>
+                            <BodyHeading size={"lg"}>The Problem</BodyHeading>
+                            <BodyHeading size={"lg"}>Challenges</BodyHeading>
+                            <BodyHeading size={"lg"}>The Solution</BodyHeading>
+                            <BodyHeading size={"lg"}>
+                                Final Thoughts
+                            </BodyHeading>
+                        </Flex>
+                    </Flex>
+                </Flex>
+            </PageWrapper>
+        </>
+    );
+};
+
+export default BoeingSpeckle;
